2292b2b182fe80f7c0f0805a6f9fd3a1a9202e46
MD SHAZAMN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2025-05-30 22:20:47 +0600

    first  commit

---
+++ src/app/(dashboard)/parents/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { parentsData } from '../../../lib/data';
+
+const ParentsPage = () => {
+  return (
+    <div>
+      parentsData
+    </div>
+  )
+}
+
+export default ParentsPage
---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ARPON School Management Dashboard",
+  description: "Next.js School Management System",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon.ico", sizes: "16x16", type: "image/x-icon" },
+      { url: "/favicon.ico", sizes: "32x32", type: "image/x-icon" },
+    ],
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+  
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
